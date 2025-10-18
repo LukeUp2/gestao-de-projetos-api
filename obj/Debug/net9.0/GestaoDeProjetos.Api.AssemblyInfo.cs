de7e34f15f285841c6e5866e0545c5fc248c72da
João Lucas <joaolucasfreitaslul@gmail.com>
2025-10-17 20:34:11 -0400

    created readme

--- obj/Debug/net9.0/GestaoDeProjetos.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/GestaoDeProjetos.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoDeProjetos.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc587c777aa3f4885542fff55ad9ebae5ab42633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc08a79ebcc46f94c05f94a6398815db36d71d6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoDeProjetos.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoDeProjetos.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
